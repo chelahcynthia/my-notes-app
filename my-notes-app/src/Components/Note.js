7b061037a97e026ef903905d0d3b73a1e4e11913
cynthia <cchelangat9892gmail.com>
2022-10-04 21:01:47 +0300

    Add Note and App changes

--- my-notes-app/src/Components/Note.js
+++ my-notes-app/src/Components/Note.js
@@ -4,6 +4,7 @@ const Note = () => {
     return (
         <div className="note">
             <span>Hello! This is our first note!</span>
+            {/* footer div that'll hold the date and delete icon */}
             <div className="note_footer"></div>
             <small>19/11/2022</small>
             <mdDeleteForever className="delete-icon" />
