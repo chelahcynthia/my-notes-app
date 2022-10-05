662e9fb753da9a68ce03e91dd1342c8a4efd6aef
cynthia <cchelangat9892gmail.com>
2022-10-05 12:01:49 +0300

    Commit Add Note

---
+++ my-notes-app/src/Components/AddNote.js
@@ -0,0 +1,9 @@
+
+const AddNote  = () => {
+    return(
+        <div className="note_new">
+            <textarea rows="8" cols="10" placeholder="Type to add a note.."
+            ></textarea>
+        </div>
+    )
+}
