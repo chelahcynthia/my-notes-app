ecfb9bda757ea111ad238dd64ffdbdf66444c74e
cynthia <cchelangat9892gmail.com>
2022-10-06 08:24:19 +0300

    Add Search to app.js

--- my-notes-app/src/App.js
+++ my-notes-app/src/App.js
@@ -1,61 +1,62 @@
-import {useEffect, useState}  from 'react';
+import { useEffect, useState } from "react";
 
 import NotesList from "./Components/Noteslist";
 import React from "react";
-import Search from './Components/Search';
-
+import Search from "./Components/Search";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
 
-useEffect(() => {
-  fetch("https://my-notes-server-app.herokuapp.com/notes")
-.then((response) => response.json())
-.then((data) => {
-  setNotes(data)
+  useEffect(() => {
+    fetch("https://my-notes-server-app.herokuapp.com/notes")
+      .then((response) => response.json())
+      .then((data) => {
+        setNotes(data);
+      });
+  }, []);
+  const fetchPost = (newNote) => {
+    fetch("https://my-notes-server-app.herokuapp.com/notes", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newNote),
+    }).then((res) => console.log(res));
+  };
 
+  const [searchText, setSearchText] = useState("");
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      text: text,
+      date: date.toDateString(),
+    };
+    fetchPost(newNote);
+    setNotes([...notes, newNote]);
+  };
+  const deleteNote = (id) => {
+    fetch(`https://my-notes-server-app.herokuapp.com/notes/${id}`, {
+    method: "DELETE"
+  })
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+  const searchNotes = (text) => {
+    const newNotes = notes.filter((note) => note.text !== text);
 
-})
-
-},[])
-
-const [searchText, setSearchText] = useState('');
-const addNote = (text) => {
-   const date = new Date();
-   const newNote = {
-    
-    text: text,
-    date: date.toDateString()
-
-   }
-   fetch("https://my-notes-server-app.herokuapp.com/notes",{
-    method: "POST",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(newNote)
-   })
-   .then((res) => console.log(res))
-setNotes([...notes, newNote])
-};
-const deleteNote = (id) => {
- const newNotes= notes.filter((note) => note.id !== id);
- setNotes(newNotes);
-}
-
+  }
 
   return (
     <div className="container">
-      <Search handleSearchNote={setSearchText}/>
-      <NotesList 
-      notes={notes} 
-      handleAddNote={addNote}
-      handleDeleteNote= {deleteNote}
+      <Search handleSearchNote={searchNotes} />
+      <NotesList
+        notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
       />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
